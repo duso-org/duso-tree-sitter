e61e4c9dd781b17b6c4413438b43e7a57122dda3
Dave Balmer <dbalmerjr@gmail.com>
2026-03-08 18:12:08 -0700

    ok try this

--- src/scanner.c
+++ src/scanner.c
@@ -1,83 +1,21 @@
 #include "tree_sitter/parser.h"
 #include <ctype.h>
-#include <string.h>
 
 enum TokenType {
   COMMENT,
 };
 
-typedef struct {
-  int depth;
-} Scanner;
-
 void *tree_sitter_duso_external_scanner_create() {
-  Scanner *scanner = malloc(sizeof(Scanner));
-  scanner->depth = 0;
-  return scanner;
+  return NULL;
 }
 
-void tree_sitter_duso_external_scanner_destroy(void *payload) {
-  free(payload);
+void tree_sitter_duso_external_scanner_destroy(void *p) {}
+
+unsigned tree_sitter_duso_external_scanner_serialize(void *p, char *buffer) {
+  return 0;
 }
 
-unsigned tree_sitter_duso_external_scanner_serialize(
-    void *payload,
-    char *buffer) {
-  Scanner *scanner = (Scanner *)payload;
-  buffer[0] = scanner->depth;
-  return 1;
-}
-
-void tree_sitter_duso_external_scanner_deserialize(
-    void *payload,
-    const char *buffer,
-    unsigned length) {
-  Scanner *scanner = (Scanner *)payload;
-  scanner->depth = length > 0 ? buffer[0] : 0;
-}
-
-static bool scan_line_comment(TSLexer *lexer) {
-  if (lexer->lookahead != '/') return false;
-
-  lexer->advance(lexer, false);
-  if (lexer->lookahead != '/') {
-    lexer->mark_end(lexer);
-    return false;
-  }
-
-  lexer->advance(lexer, false);
-  while (lexer->lookahead != '\n' && lexer->lookahead != '\0') {
-    lexer->advance(lexer, false);
-  }
-
-  return true;
-}
-
-static bool scan_block_comment(TSLexer *lexer) {
-  if (lexer->lookahead != '/') return false;
-
-  lexer->advance(lexer, false);
-  if (lexer->lookahead != '*') {
-    lexer->mark_end(lexer);
-    return false;
-  }
-
-  lexer->advance(lexer, false);
-
-  while (lexer->lookahead != '\0') {
-    if (lexer->lookahead == '*') {
-      lexer->advance(lexer, false);
-      if (lexer->lookahead == '/') {
-        lexer->advance(lexer, false);
-        return true;
-      }
-    } else {
-      lexer->advance(lexer, false);
-    }
-  }
-
-  return true;
-}
+void tree_sitter_duso_external_scanner_deserialize(void *p, const char *b, unsigned n) {}
 
 bool tree_sitter_duso_external_scanner_scan(
     void *payload,
@@ -88,21 +26,39 @@ bool tree_sitter_duso_external_scanner_scan(
     return false;
   }
 
-  // Skip whitespace
   while (isspace(lexer->lookahead)) {
     lexer->advance(lexer, true);
   }
 
-  if (scan_line_comment(lexer)) {
-    lexer->result_symbol = COMMENT;
-    return true;
-  }
+  if (lexer->lookahead == '/' ) {
+    int start_pos = lexer->get_column(lexer);
+    lexer->advance(lexer, false);
 
-  lexer->mark_end(lexer);
+    if (lexer->lookahead == '/') {
+      lexer->advance(lexer, false);
+      while (lexer->lookahead != '\n' && lexer->lookahead != '\0') {
+        lexer->advance(lexer, false);
+      }
+      lexer->result_symbol = COMMENT;
+      return true;
+    }
 
-  if (scan_block_comment(lexer)) {
-    lexer->result_symbol = COMMENT;
-    return true;
+    if (lexer->lookahead == '*') {
+      lexer->advance(lexer, false);
+      while (lexer->lookahead != '\0') {
+        if (lexer->lookahead == '*') {
+          lexer->advance(lexer, false);
+          if (lexer->lookahead == '/') {
+            lexer->advance(lexer, false);
+            break;
+          }
+        } else {
+          lexer->advance(lexer, false);
+        }
+      }
+      lexer->result_symbol = COMMENT;
+      return true;
+    }
   }
 
   return false;
